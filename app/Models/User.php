0047511b053938316449c4c17d148b95b10462be
Cristian López Ramirez <cl827209@gmail.com>
2022-02-18 15:51:10 -0600

    Creación de relaciones entre modelos

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,12 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
         'email',
         'password',
+        'role',
+        'description',
+        'image'
     ];
 
     /**
@@ -41,4 +45,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 }
